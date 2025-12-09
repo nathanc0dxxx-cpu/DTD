73aa466ac6111f1c8e4fceea49c35d6925bbd576
nathanc0dxxx-cpu <nathanc0dxxx@gmail.com>
2025-12-08 20:29:31 -0400

    Atualizar o DTDPackManager.lua

--- DTDPackManager.lua
+++ DTDPackManager.lua
@@ -1,1 +1,61 @@
-gg
+
+print("\27[0m\27[44m[DTD::PM]:\27[0m \27[93minitializing...\27[0m")
+
+_G.DTDPackManager = {
+  
+  pack = "PlaceHolder",
+  store = [[
+  PlaceHolder.txt>>prate=5 owner=Dougla037p<<>>c
+  PLACEHOLDER TEXT
+  c<<
+  ]],
+  install = function(self)
+    print("\27[44m[DTD::PM]:\27[0m \27[93mparsing pack...\27[0m")
+    local mainpack = ""
+    for v in self.store:gmatch("(.-)%s*>>p%s*.-%s*p<<") do
+      if v:match(self.pack) then
+        v = v:gsub(">>p%s*(.-)%s*p<<","")
+        mainpack = v
+        print("\27[44m[DTD::PM]:\27[0m \27[92mfound:\n\27[0m"..v)
+      end
+    end
+    local packcont = ""
+    for v in self.store:gmatch("(.-%s*>>p%s*.-%s*p<<>>c%s*.-%s*c<<)") do
+      local packmatch = v:match("(.-)%s*>>p%s*.-%s*p<<")
+      if packmatch:match(self.pack) then
+        local get = v:match(".-%s*>>p%s*.-%s*p<<>>c%s*(.-)%s*c<<")
+        print("\27[44m[DTD::PM]:\27[0m \27[92mcontent:\n\27[0m"..get)
+        packcont = get
+      end
+    end
+    print("\27[44m[DTD::PM]:\27[0m \27[93minstalling...\27[0m")
+    if mainpack then
+      if packcont then
+        local package = io.open(mainpack, "r")
+        if package then
+          package:write(packcont)
+          print("\27[44m[DTD::PM]:\27[0m \27[93mupdated pack: \n"..mainpack.."\n\27[92m at: ")
+          os.execute("pwd")
+        else
+          package = io.open(mainpack, "w")
+          package:write(packcont)
+          print("\27[44m[DTD::PM]:\27[0m \27[92minstalled pack: \n"..mainpack.."\n\27[92m at: ")
+          os.execute("pwd")
+        end
+      end
+    else
+      print("\27[44m[DTD::PM]:\27[0m \27[91mnot found!")
+    end
+    
+    print("\27[0m\27[44m[DTD::PM]:\27[0m \27[93mfinishing session...\27[0m")
+    if self then
+      for i in ipairs(self) do
+        self[i] = nil
+      end
+      _G.DTDPackManager = nil
+    end
+    print("\27[44m[DTD::PM]:\27[0m \27[96mfinished")
+  end,
+}
+
+print("\27[44m[DTD::PM]:\27[0m \27[92minjected DTDPackManager\27[0m")
