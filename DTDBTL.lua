4ae36551310b97ce90b68595e54d60155e87c5e7
nathanc0dxxx-cpu <nathanc0dxxx@gmail.com>
2025-12-17 22:52:37 -0400

    Atualizar o DTDBTL.lua

--- DTDBTL.lua
+++ DTDBTL.lua
@@ -1,4 +1,4 @@
-print("\27[44m[DTD::BOOTLOADER]:\27[0m \27[93minitializing...\27[0m")
-local http = io.popen("curl -s https://raw.githubusercontent.com/nathanc0dxxx-cpu/DTD/main/DTDBTS.lua")
-local content = http:read("*a") http:close()
-if content then load(content)() else print("\27[44mDTD::BOOTLOADER]:\27[0m \27[91mfailed to load") end
+print("\27[44m[DTD::BOOTLOADER]:\27[0m \27[93minitializing...\27[92m")
+local h=io.popen("curl -s https://raw.githubusercontent.com/nathanc0dxxx-cpu/DTD/main/DTDBTS.lua")
+local c=h:read("*a")h:close()
+if c~="" then load(c)()else print("\27[0m\27[44m[DTD::BOOTLOADER]:\27[0m \27[91mfailed to load\nserver could be instable or no \ninternet connection")end
