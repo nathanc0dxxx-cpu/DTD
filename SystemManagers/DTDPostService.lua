cca1b9797cbb2fba8f5cab22310c2dd9ccf5b8d6
Dougla037 <nathanc0dxxx@gmail.com>
2026-01-21 00:16:27 -0400

    Atualizar o DTDPostService.lua

--- SystemManagers/DTDPostService.lua
+++ SystemManagers/DTDPostService.lua
@@ -30,8 +30,7 @@ function base64(data)
 end
 
 local function getSHA(url, token)
-    local handle = io.popen(string.format('
-        curl -s -H "Authorization: token %s" %s', token, url))
+    local handle = io.popen(string.format('curl -s -H "Authorization: token %s" %s', token, url))
     local result = handle:read("*a")
     handle:close()
     local sha = result:match('"sha"%s*:%s*"([%w]+)"')
