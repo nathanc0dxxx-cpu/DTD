44dd20917a7fc6b85a8e5697af5d449ddf9cc03b
tutugrande1235-DTD <jr16102022@gmail.com>
2026-01-22 14:16:31 -0400

    Atualizar o DTDServersManager.lua

--- Manager/DTDServersManager.lua
+++ Manager/DTDServersManager.lua
@@ -1,29 +1,32 @@
 os.execute("clear")
 print("\27[0m\27[44m[DTD::SM]:\27[0m \27[93minitializing...")
 if not DTDUser then _G.DTDUser = { name = "User" } end
-local serverstxt = nil
+
+local serversjson = nil
+local servers = {}
 local function loadservers()
 local ss = io.popen("curl -s https://api.github.com/repos/nathanc0dxxx-cpu/DTD/contents/Servers")
-serverstxt = ss:read("*a") ss:close()
-end loadservers()
+serversjson = ss:read("*a") ss:close()
 
+servers = {}
+for i,v in serversjson:gmatch("%s*\"name\"%s*:%s*\"(.-)%s*@(.-)\"") do
+    local obj = { name = i, owner = v }
+    table.insert(servers, obj)
+end
+end
 
-local ssa = io.popen("curl -v https://raw.githubusercontent.com/nathanc0dxxx-cpu/DTD/main/SystemManagers/DTDPostService.lua")
+local ssa = io.popen("curl -s https://raw.githubusercontent.com/nathanc0dxxx-cpu/DTD/main/SystemManagers/DTDPostService.lua")
 local ssac = ssa:read("*a")
-ssa:close() print(ssac)
+ssa:close()
 local ssacl = load(ssac)()
+
+loadservers()
 
 ::s::
 
 os.execute("clear")
 print("\27[0m\27[44m[DTD::SERVERS]:\27[41m[=======================]")
 ----------
-
-local servers = {}
-for i,v in serverstxt:gmatch("%s*\"name\"%s*:%s*\"(.-)%s*@(.-)@\"") do
-    local obj = { name = i, owner = v }
-    table.insert(servers, obj)
-end
 
 header = ""
 for i,v in ipairs(servers) do
@@ -68,7 +71,7 @@ elseif inp == "create" then
             session = false
         elseif cinp == "new" then
             os.execute("clear")
-            print("\27[44m[HOST/NAME]:\27[0m")
+            print("\27[0m\27[44m[HOST/NAME]:\27[0m")
             io.write("> \27[92m")
             local name = io.read()
             local can = true
@@ -80,10 +83,12 @@ elseif inp == "create" then
             end
 
             if can == true then
+                print("\27[0m\27[44m[URL]:\27[0m")
+                io.write("> \27[92m")
                 local url = io.read()
                 if url then
                     print("\27[93mworking...\27[0m")
-                    local strucn = name.."@".._G.DTDUser.name.."@"
+                    local strucn = name.."@".._G.DTDUser.name
                     strucn = strucn:gsub("\n",""):gsub(" ","")
                     DTDPostService:servers(url, strucn, "POST")
                     os.execute("clear")
@@ -115,17 +120,18 @@ elseif inp == "create" then
             print("\27[44m[===============================]\27[0m")
             io.write("> \27[92m")
             local nameh = io.read()
-            if nameh:sub(1,1) ~= " " or nameh:sub(1,1) ~= "" then
+            if nameh:sub(1,1) ~= " " and nameh:sub(1,1) ~= "" then
                 for i,v in ipairs(servers) do
                     if nameh == v.name and _G.DTDUser.name == v.owner then
                         print("\27[93mworking...\27[0m")
-                        DTDPostService:servers("",v.name.."@".._G.DTDUser.name.."@", "DELETE")
+                        DTDPostService:servers("",v.name.."@".._G.DTDUser.name, "DELETE")
                         os.execute("clear")
                         session2 = false
                         print("\27[92mfinished! \27[93mloading servers...\27[0m")
-                        os.execute("sleep 1")
+                        os.execute("sleep 3")
                         loadservers()
-                    end if session2 == true then
+                        break
+                    end if session2 == true and (nameh ~= v.name or _G.DTDUser.name ~= v.owner) then
                         print("\27[91mserver not found.\27[0m")
                     end
                 end
