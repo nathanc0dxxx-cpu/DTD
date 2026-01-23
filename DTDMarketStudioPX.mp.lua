b4464202472c3affd8ad1f03e9ae55dd940eca0c
Dougla037 <nathanc0dxxx@gmail.com>
2026-01-22 23:47:59 -0400

    Atualizar o DTDMarketStudioPX.mp.lua

--- DTDMarketStudioPX.mp.lua
+++ DTDMarketStudioPX.mp.lua
@@ -2,7 +2,7 @@ std:newcmd({
   token = "help",
   func = function()
     for i,v in ipairs(std.cmd) do
-      print("\27[96m"..i.." \27[92m"..v.token.."\n  \27[93m"..v.desc)
+      print("\27[96m"..i.." \27[92m"..v.token.."\n  \27[93m"..v.desc.."\27[0m\n")
     end
   end,
   desc = "it helps you bro... JUST!"
