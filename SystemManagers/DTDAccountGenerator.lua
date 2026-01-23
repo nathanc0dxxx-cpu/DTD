fcf55746e4afd2771abf6debc39eabd449cec966
tutugrande1235-DTD <jr16102022@gmail.com>
2026-01-23 16:06:14 -0400

    Criar DTDAccountGenerator.lua

---
+++ SystemManagers/DTDAccountGenerator.lua
@@ -0,0 +1,55 @@
+function simple_hash(password, salt)
+    local hash = 0
+    local data = password .. salt
+
+    for i = 1, #data do
+        local c = data:byte(i)
+        hash = (hash * 31 + c) % 4294967296
+    end
+
+    return string.format("%08x", hash)
+end
+
+function generate_salt(length)
+    local charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    local salt = ""
+    for i = 1, length do
+        local rand = math.random(1, #charset)
+        salt = salt .. charset:sub(rand, rand)
+    end
+    return salt
+end
+
+function genuser(name, pass)
+    local ss = io.popen("curl -s https://raw.githubusercontent.com/nathanc0dxxx-cpu/DTD/main/SystemManagers/DTDPostService.lua")
+    if ss then load(ss:read("*a"))() ss:close() else return nil end
+    local ssa = io.popen("curl -s https://api.github.com/repos/nathanc0dxxx-cpu/DTD/contents/Accounts")
+    local ssac = nil
+    if ssa then ssac = ssa:read("*a") ssa:close() else return nil end
+    local users = {}
+    for v in ssac:gmatch("%s*\"name\"%s*:%s*\"(.-)\"") do
+        table.insert(users, v)
+    end 
+    local id = 0
+    local sse = io.popen("curl -s https://raw.githubusercontent.com/nathanc0dxxx-cpu/DTD/main/Accounts/"..users[#users])
+    if sse then
+        local tab = {} 
+        for v in sse:read("*a"):gmatch("%S+") do
+            table.insert(tab, v)
+        end ss:close()
+        id = tonumber(tab[2]) + 1
+    else
+        return nil
+    end for i,v in ipairs(users) do
+        if v == name then return nil end
+    end
+    
+    math.randomseed(os.time())
+    local salt = generate_salt(16)
+    local hash = simple_hash(pass, salt)
+    
+    local content = hash .. "\n" .. id .. "\n" .. salt
+    DTDPostService:accounts(content, name, "POST")
+    
+    return { un = name, ps = pass }
+end
