8fb4ea921b8c440d81c30063449acf44632f0609
Dougla037 <nathanc0dxxx@gmail.com>
2026-01-23 00:11:01 -0400

    Atualizar o DTDMarketStudioPX.mp.lua

--- DTDMarketStudioPX.mp.lua
+++ DTDMarketStudioPX.mp.lua
@@ -7,3 +7,35 @@ std:newcmd({
   end,
   desc = "it helps you bro... JUST!"
 })
+std:newcmd({
+    token = "deploy",
+    func = function()
+    end,
+    desc = "deploy a file to the market"
+})
+std:newcmd({
+    token = "view",
+    func = function()
+        local ss = io.popen("curl -s https://api.github.com/repos/nathanc0dxxx-cpu/DTD/contents/Market")
+        local handle = ss:read("*a")
+        ss:close() local pkgs = {}
+        for i,v in handle:gmatch("%s*\"name\"%s*:%s*\"(.-)@(.-)\"") do
+            local obj = { name = i, owner = v }
+            table.insert(pkgs, obj)
+        end for i,v in ipairs(pkgs) do
+            if v.owner == _G.DTDUser.name then
+                print("\27[92m [ \27[96m"..v.name.."\27[92m ]\27[0m")
+            end
+        end
+    end,
+    desc = "view your deployed files"
+})
+std:newcmd({
+    token = "ls",
+    func = function()
+        local pip = io.popen("ls")
+        print("\27[96mcurrent files:\n\27[0m  "..pip:read("*a"))
+        pip:close()
+    end,
+    desc = "list your local files"
+})
