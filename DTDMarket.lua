12b7d4e5f1bde76e79c95dc992739e4f573fbb82
Dougla037 <nathanc0dxxx@gmail.com>
2026-01-25 15:00:22 -0400

    Atualizar o DTDMarket.lua

--- DTDMarket.lua
+++ DTDMarket.lua
@@ -1,6 +1,9 @@
 
 os.execute("clear")
 local exit = false
+local hubs = false
+local markets = true
+local uinp = ""
 local packs = {}
 local function loadpacks()
     local ss = io.popen("curl -s https://api.github.com/repos/nathanc0dxxx-cpu/DTD/contents/Market")
@@ -15,36 +18,6 @@ end loadpacks()
 
 local inp = nil
 
-local function market()
-    os.execute("clear")
-    
-    print("\27[0m\27[44m[Market]:\27[41m[=============]\27[0m\n")
-    local maxi = 3
-    for i = 1,9 do
-        if packs[i] then
-        io.write("\27[92m [\27[93m"..packs[i].name.."\27[92m]\27[0m")
-        if i > maxi then
-            maxi = maxi + 3
-            print("\n")
-        end end
-    end
-    print("\n\n\27[44m[======================]\27[0m")
-    io.write(" > \27[90m<pack>, search, exit, reset\27[27D\27[92m")
-    inp = io.read()
-    if inp == "exit" then
-        os.execute("clear")
-        print("\27[91mlogout\27[0m")
-        return
-    elseif inp == "reset" then
-        loadpacks()
-        market()
-    elseif inp:sub(1,7) == "search " then
-        search(inp:sub(8))
-    else
-        hub(inp)
-    end
-end
-
 local function search(query)
     os.execute("clear")
     print("\27[0m\27[44m[Results]:\27[41m[============]\27[0m\n")
@@ -56,9 +29,45 @@ local function search(query)
     io.write(" > \27[90m<pack>, back\27[12D\27[92m")
     inp = io.read()
     if inp == "back" then
-        market()
+        markets = true
+        return
     else
-        hub(inp)
+        hubs = true
+        return
+    end
+end
+
+local function market()
+    os.execute("clear")
+    
+    print("\27[0m\27[44m[Market]:\27[41m[=============]\27[0m\n")
+    local maxi = 2
+    for i = 1,9 do
+        if packs[i] then
+            io.write("\27[92m [\27[93m"..packs[i].name:gsub("%.%s*(.-)$","").."\27[92m]\27[0m")
+            if i >= maxi then
+                maxi = maxi + 2
+                print("\n")
+            end
+        end
+    end
+    print("\n\n\27[44m[======================]\27[0m")
+    io.write(" > \27[90m<pack>, search, exit, reset\27[27D\27[92m")
+    inp = io.read()
+    if inp == "exit" then
+        os.execute("clear")
+        print("\27[91mlogout\27[0m")
+        exit = true
+        return
+    elseif inp == "reset" then
+        loadpacks()
+        markets = true
+        return
+    elseif inp:sub(1,7) == "search " then
+        search(uinp:sub(8))
+    else
+        hubs = true
+        return
     end
 end
 
@@ -70,7 +79,8 @@ local function hub(query)
             obj = v
             break
         end
-    end if not obj.name then market() end obj.desc = "\27[0m no description provided!"
+    end if not obj.name then markets = true return end
+    obj.desc = "\27[0m no description provided!"
     print("\27[0m\27[44m[HUB]:\27[41m[==========]\27[0m")
     print(string.format([[
   .--_____
@@ -82,9 +92,10 @@ local function hub(query)
     ]], "\27[96m"..obj.name.."\27[0m", "\27[90mby ", obj.owner.."\27[0m", obj.desc))
     print("\27[0m\27[44m[================]\27[0m")
     io.write(" > \27[90mback, install\27[13D\27[92m")
-    local uinp = io.read()
+    uinp = io.read()
     if uinp == "back" then
-        market()
+        markets = true
+        return
     elseif uinp == "install" then
         os.execute("clear")
         print("\27[93mdownloading...\27[0m")
@@ -100,15 +111,26 @@ local function hub(query)
                 print("\27[92minstalled!\n\27[0mpath: ")
                 os.execute("pwd")
                 os.execute("sleep 3")
-                hub(query)
+                hubs = true
+                return
             else
                 print("\27[91merror while downloading...\27[0m")
             end
         end
-        market()
+        markets = true
+        return
     else
-        hub(query)
+        hubs = true
+        return
     end
 end
 
-market()
+while not exit do
+    if markets == true then
+        markets = false
+        market()
+    elseif hubs == true then
+        hubs = false
+        hub(uinp)
+    end
+end
