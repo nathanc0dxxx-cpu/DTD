6b81bbd7549aee1b5e59e61d51d6257c3498950b
tutugrande1235-DTD <jr16102022@gmail.com>
2026-02-18 17:45:37 -0400

    Atualizar o DTDPostService.lua

--- SystemManagers/DTDPostService.lua
+++ SystemManagers/DTDPostService.lua
@@ -1,12 +1,4 @@
-local function json(str)
-    str = str:gsub("\\", "\\\\")
-    str = str:gsub("\"", "\\\"")
-    str = str:gsub("\n", "\\n")
-    str = str:gsub("\r", "\\r")
-    str = str:gsub("\t", "\\t")
-    str = str:gsub("\'","\\\'")
-    return str
-end
+
 
 local function post(url, cont, mode)
     local fil = io.open("DTDUser","r")
@@ -24,10 +16,9 @@ local function post(url, cont, mode)
     end
     local cc = cont
     print("\27[93madapting json...\27[0m")
-    cc = json(cc)
 
     local token = "ghp_VFb1kXtYJ68rDzuhn41f2xbcegago30naUb".."f"
-    os.execute(string.format('curl -s -X POST -H "Accept: application/vnd.github+json" -H "Authorization: token %s" https://api.github.com/repos/tutugrande1235-DTD/DTD-Source-Scripts/dispatches -d \'{"event_type": "post", "client_payload": {"user": "%s", "pass": "%s", "mode": "%s", "file": "%s", "content": "%s"}}\'', token, DTDUser.name, c, mode, url, cc))
+    os.execute(string.format('curl -s -X POST -H "Accept: application/vnd.github+json" -H "Authorization: token %s" https://api.github.com/repos/tutugrande1235-DTD/DTD-Source-Scripts/dispatches -d \'{"event_type": "post", "client_payload": {"user": "%s", "pass": "%s", "mode": "%s", "file": "%s", "content": %q}}\'', token, DTDUser.name, c, mode, url, cc))
 end
 
 _G.DTDPostService = {
