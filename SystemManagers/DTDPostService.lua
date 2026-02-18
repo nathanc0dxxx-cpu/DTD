66aaad699964177a17c875cd25e3d9e86efa9cd3
Dougla037 <nathanc0dxxx@gmail.com>
2026-02-17 21:08:03 -0400

    Atualizar o DTDPostService.lua

--- SystemManagers/DTDPostService.lua
+++ SystemManagers/DTDPostService.lua
@@ -27,22 +27,7 @@ local function post(url, cont, mode)
     cc = json(cc)
 
     local token = "ghp_VFb1kXtYJ68rDzuhn41f2xbcegago30naUb".."f"
-    os.execute(string.format([[
-      curl -s -X POST \
-      -H "Accept: application/vnd.github+json" \
-      -H "Authorization: token %s" \
-      https://api.github.com/repos/tutugrande1235-DTD/DTD-Source-Scripts/dispatches \
-      -d '{
-        "event_type": "post",
-        "client_payload": {
-          "user": "%s",
-          "pass": "%s",
-          "mode": "%s",
-          "file": "%s",
-          "content": "%s"
-        }
-      }'
-   ]], token, DTDUser.name, c, mode, url, cc))
+    os.execute(string.format('curl -s -X POST -H "Accept: application/vnd.github+json" -H "Authorization: token %s" https://api.github.com/repos/tutugrande1235-DTD/DTD-Source-Scripts/dispatches -d \'{"event_type": "post", "client_payload": {"user": "%s", "pass": "%s", "mode": "%s", "file": "%s", "content": "%s"}}\'', token, DTDUser.name, c, mode, url, cc))
 end
 
 _G.DTDPostService = {
