d21a05681544e463d9741a2996292e7a54b2809a
tutugrande1235-DTD <jr16102022@gmail.com>
2026-02-24 18:00:40 -0400

    Atualizar o install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 printf "\e[93mcreating file: DOT.lua\n"
 printf "installing dependencies...\e[0m\n"
-pkg install lua54
+pkg install -y lua54
 alias dtdos="lua54 DOT.lua"
 curl -fsSL https://raw.githubusercontent.com/nathanc0dxxx-cpu/DTD/main/DTDBTL.lua > DOT.lua
 stty -echo -icanon
