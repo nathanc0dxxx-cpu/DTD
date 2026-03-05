e4fdd9d7b6db770ee71ba57c2efd2f7d44d8143c
tutugrande1235-DTD <jr16102022@gmail.com>
2026-03-04 22:44:05 -0400

    Atualizar o install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,25 @@
-printf "\e[93mcreating file: DOT.lua\n"
+printf "\e[93mINSTALLING NOW: DOT APP (DOT.lua)\n"
 printf "installing dependencies...\e[0m\n"
-pkg install -y lua54
-
-if ! grep -q 'alias dtdos=' ~/.bashrc; then
-    echo 'alias dtdos="lua $HOME/DOT.lua"' >> ~/.bashrc
+if ! command -v lua >/dev/null 2>&1; then
+    pkg install -y lua54
 fi
 
-curl -fsSL https://raw.githubusercontent.com/nathanc0dxxx-cpu/DTD/main/DTDBTL.lua > ~/DOT.lua
-source ~/.bashrc
+maind="${PREFIX:-/usr}/bin"
+mkdir -p "$maind"
+if [ ! -x "$maind/lua" ]; then
+    printf "\e[91mERROR: \e[0mlua package installation failed!\n"
+    exit 1
+fi
+
+printf "\e[93minstalling program...\n"
+
+curl -fsSL https://raw.githubusercontent.com/nathanc0dxxx-cpu/DTD/main/DTDBTL.lua -o "$maind/DOT.lua" || {
+    printf "\e[91mERROR: \e[0mfailed while installing app\n"
+    exit 1
+}
+echo "lua $maind/DOT.lua" > "$maind/dtdos"
+chmod +x "$maind/dtdos"
+
 stty -echo -icanon
 printf "\e[32m[PROCCESS COMPLETED | Press Enter]:\e[0m"
 read
